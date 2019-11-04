ba875411488f902e55dec5792bfac35c02967a77
Isida Zyun'iti <zyun@moin.jp>
2019-11-04 13:59:55 +0900

    Add word random.

--- src/index.ts
+++ src/index.ts
@@ -3,21 +3,34 @@ import mongoose from "mongoose";
 import { Word } from "./models/Word";
 
 const mongoUrl = process.env.MONGODB_URI || 'mongodb://localhost/your-app-name';
-
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true });
 
-const ore = new Word({emoji: "tada", text: "hey hey hey."});
+const wordList = [
+  "狭き門より入れ。",
+  "人はパンのみにて生くるにあらず。",
+  "剣を取る者は、剣で滅びる。",
+  "明日のことを思いわずらうな。明日のことは、明日自身が思いわずらうであろう。",
+  "求めよ、そうすれば、与えられるであろう。",
+];
 
-Word.findOne({ text: "hey hey hey." }, (err, existingWord) => {
-  if (err) { console.log(err)  }
-  if (existingWord) {
-    console.log(existingWord);
-  } else {
-    ore.save(err);
-  }
+wordList.forEach(word => {
+  const tada = new Word({emoji: "tada", text: word});
+  Word.findOne({ text: word}, (err, existingWord) => {
+    if (err) { console.log(err)  }
+    if (!existingWord) {
+      tada.save(err);
+    }
+  });
 });
 
 const PORT = process.env.PORT || 3000;
 const app = express();
 app.get('/', (req, res) => res.send('Hello World!'));
 app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`));
+
+Word.countDocuments({emoji: 'tada'}).then((count: any) => {
+  const wordRndr = Math.floor(Math.random() * count);
+  Word.find({emoji: "tada"}).limit(1).skip(wordRndr).then(d =>{
+    console.log(d);
+  });
+});
